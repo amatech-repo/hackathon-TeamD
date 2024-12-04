b95f87be38290751959b637dff428da209b23c93
Sota Tsunemine <motarou.engineer@gmail.com>
2024-12-04 18:14:29 +0900

    build: indexの中身とrouteの内容をちょっと変更した

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,21 @@
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { app } from "@server/controller/controller";
 
-app.basePath("/api");
+const serverApp = new OpenAPIHono().basePath("/api");
+serverApp.route("/", app);
+if (process.env.NODE_ENV !== "production") {
+  serverApp.doc("/doc", {
+    info: {
+      title: "An API",
+      version: "v1",
+    },
+    openapi: "3.1.0",
+  });
 
-export default app;
+  // Swagger UIエンドポイント
+  serverApp.get("/ui", swaggerUI({ url: "/api/doc" }));
+}
+serverApp.basePath("/api");
+
+export default serverApp;
